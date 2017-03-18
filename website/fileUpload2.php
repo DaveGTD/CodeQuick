54245e908fb735c45d13573126bbccb22eded012
Dave Aditya <adave@adobe.com>
2017-03-18 11:03:51 -0600

    auto

--- website/fileUpload2.php
+++ website/fileUpload2.php
@@ -8,5 +8,38 @@
     <input type="file" id="file" name="files[]" multiple="multiple" />
   <input type="submit" value="Upload!" />
 </form>
+
+
+<script>
+
+//get the input and UL list
+var input = document.getElementById('file');
+var list = document.getElementById('fileList');
+
+//empty list for now...
+while (list.hasChildNodes()) {
+	list.removeChild(ul.firstChild);
+}
+
+//for every file...
+for (var x = 0; x < input.files.length; x++) {
+	//add to list
+	var li = document.createElement('li');
+	li.innerHTML = 'File ' + (x + 1) + ':  ' + input.files[x].name;
+	list.append(li);
+}
+
+</script>
+
+
+
+<br>
+<br>
+
+<div id="fileList">
+</div>
+
+
+
 </body>
 </html>
